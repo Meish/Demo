06a90d32964475c58c96eb2ea3a0ed04422207c7
Meishar Shahoha <meish123@gmail.com>
2022-02-16 11:43:14 +0200

    added function,tests, makefile

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+def addition(x,y):
+    return x+y
+def substraction(x,y):
+    return x-y
+
+def main():
+    print(f"1 + 2 = {addition(1,2)}")
+    print(f"3 - 1 = {substraction(3,1)}")
+
+if __name__ == '__main__':
+    main()
--- test_app.py
+++ test_app.py
@@ -1,1 +1,20 @@
+from app import addition, substraction
+
+def test_addition():
+    assert(addition(3,4) == 7)
+    assert(addition(3,2) == 5)
+    assert(addition(0,4) == 4)
+    print("test1 - passed")
+
+def test_substraction():
+    assert(substraction(3,2) == 1)
+    assert(substraction(3,1) == 2)
+    print("test2 - passed")
+
+def main():
+    test_substraction()
+    test_addition()
+
+if __name__ == '__main__':
+    main()
 
